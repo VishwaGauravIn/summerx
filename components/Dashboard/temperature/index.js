51a9274b191df9763c6a60d0b20af2abadfad8de
Vishwa Gaurav <vishwagauravin@gmail.com>
2022-06-23 20:39:13 +0530

    weather api testing

--- components/Dashboard/temperature/index.js
+++ components/Dashboard/temperature/index.js
@@ -1,6 +1,14 @@
+import axios from 'axios';
 import React from 'react'
 
 export default function Temperature() {
+  axios
+    .get(
+      'https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid={VGIN}'
+    )
+    .then((res) => {
+      console.log(res)
+    });
   return (
     <div>Temperature</div>
   )
