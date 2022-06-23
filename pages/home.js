0cc7ca49185ed7186c37fb662785cbf6049ac948
Vishwa Gaurav <vishwagauravin@gmail.com>
2022-06-23 15:36:14 +0530

    minor fixes

--- pages/home.js
+++ pages/home.js
@@ -1,10 +1,18 @@
+import Head from "next/head";
 import React from "react";
 import Header from "../components/Header";
 
 export default function Home() {
   return (
-    <main className="p-6 text-primary dark:text-secondary transition-all ease-in-out">
-      <Header />
-    </main>
+    <>
+      <Head>
+        <title>SummerX - Home</title>
+        <meta name="description" content="SummerX Website" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="p-6 text-primary dark:text-secondary transition-all ease-in-out">
+        <Header />
+      </main>
+    </>
   );
 }
