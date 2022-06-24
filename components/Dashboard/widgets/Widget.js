36d89777c8c7f578a76f4dabcd1bafd1bc7230f7
Vishwa Gaurav <81325730+VishwaGauravIn@users.noreply.github.com>
2022-06-24 23:28:14 +0530

    UI impv

--- components/Dashboard/widgets/Widget.js
+++ components/Dashboard/widgets/Widget.js
@@ -4,7 +4,7 @@ export default function Widget({ icon, label, onClick }) {
   return (
     <div className="group w-28 h-28 rounded-full flex flex-col mx-6 sm:mx-8 lg:mx-6 xl:mx-8 mb-[72px] justify-center items-center dark:text-primary text-secondary bg-primary dark:bg-secondary transition-all ease-in-out ring-8 ring-primary/20 dark:ring-secondary/20 relative" onClick={onClick}>
       {icon}
-      <p className="absolute w-max -bottom-11 text-xl text-primary dark:text-secondary group-hover:flex font-medium">
+      <p className="absolute w-max -bottom-11 text-base sm:text-xl text-primary dark:text-secondary group-hover:flex font-medium">
         {label}
       </p>
     </div>
