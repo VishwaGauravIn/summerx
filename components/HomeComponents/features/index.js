490dd8f3dffdedf0554c5758203cf531f3d32d22
Vishwa Gaurav <81325730+VishwaGauravIn@users.noreply.github.com>
2022-06-24 19:29:32 +0530

    imperoved features section

--- components/HomeComponents/features/index.js
+++ components/HomeComponents/features/index.js
@@ -3,43 +3,33 @@ import { Tick } from "../../elements/SVGs";
 
 export default function Features() {
   return (
-    <div>
-      <div className="text-center text-xl lg:text-3xl xl:text-4xl mt-12 lg:mt-24 xl:mt-36  ">
+    <div className="flex flex-col">
+      <div className="text-center text-xl lg:text-3xl xl:text-4xl mt-12 lg:mt-24 xl:mt-36 flex flex-col">
         No Need To Worry!
         <p className="font-bold text-primary-900  dark:text-secondary-200 my-4 text-3xl lg:text-6xl">
           Here is Best and Simple Solution
         </p>
-        <div className="grid grid-cols-1 md:grid-cols-2 justify-center items-center gap-4 lg:gap-6 xl:gap-8 w-[90%] mt-8 mx-auto lg:mt-20">
-          {[
-            "Heat wave warning",
-            "Reminder",
-            "Medical Advice",
-            "Community Support",
-            "Weather ",
-            "Music",
-            "Lot more Features",
-          ].map((name) => (
-            <Box key={name}>{name}</Box>
-          ))}
-          <p className="flex flex-row items-start gap-4 ">All for FREE!</p>
+        <div className="flex justify-center flex-wrap w-9/12 sm:w-10/12 md:w-8/12 self-center py-2 sm:py-4 md:py-6 lg:py-8">
+          {feature("Heat wave warning")}
+          {feature("Reminder")}
+          {feature("Medical Support")}
+          {feature("Weather Report")}
+          {feature("Music")}
+          {feature("Plant a Tree")}
+          {feature("Explore Nearby")}
+          {feature("Community Support")}
         </div>
       </div>
-      <div className="my-12 py-8 text-center">
-        <a
-          href="#"
-          className="no-underline text-3xl xl:text-4xl  px-8 py-4 rounded-full text-secondary-500 bg-primary-500 dark:bg-secondary-500 dark:text-primary-500 text-center outline outline-4 outline-secondary-600 dark:outline-primary-400 "
-        >
-          Explore Babe!
-        </a>
-      </div>
+      {/* TODO: Add Get Started */}
     </div>
   );
 }
-function Box({ children }) {
+
+const feature = (text) => {
   return (
-    <div className="flex flex-row items-start gap-4 ">
+    <p className="flex flex-row items-start gap-4 w-full sm:w-1/2 mb-2">
       <Tick className="w-6 md:w-8 lg:w-12" />
-      <div>{children}</div>
-    </div>
+      {text}
+    </p>
   );
-}
+};
