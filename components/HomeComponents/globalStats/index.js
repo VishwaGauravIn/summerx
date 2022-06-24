0a6eb11c5f774450f8a28bf17056882c1734a443
Vishwa Gaurav <81325730+VishwaGauravIn@users.noreply.github.com>
2022-06-24 20:38:00 +0530

    UI impv

--- components/HomeComponents/globalStats/index.js
+++ components/HomeComponents/globalStats/index.js
@@ -1,23 +1,27 @@
 import React from "react";
-import { Chart, Fall, Person_Left } from "../../elements/SVGs";
+import { ChartBarIcon, TrendingDownIcon, UserGroupIcon, UserRemoveIcon } from '@heroicons/react/solid'
 
 export default function GlobalStats() {
   return (
     <>
-      <div className="text-center text-xl lg:text-3xl xl:text-4xl my-12 lg:my-24 xl:my-36 font-semibold">
+      <div className="text-center text-xl lg:text-3xl xl:text-4xl mt-12 lg:mt-24 xl:mt-36 font-semibold">
         What&apos;s happening around globe due to Summer (per year)
         <div className="flex flex-col md:flex-row justify-center items-center md:items-stretch w-[90%] mt-8 mx-auto flex-wrap  lg:mt-20">
           <Box>
-            <Person_Left className="w-[70%] mx-auto stroke-[1.6]" />
-            160K+ lost their lives
+            <UserRemoveIcon className="w-[70%] mx-auto stroke-[1.6]" />
+            166K+ lost their lives
           </Box>
           <Box>
-            <Fall className="w-[70%] mx-auto  stroke-[1.6]" />
+            <TrendingDownIcon className="w-[70%] mx-auto" />
             5% fall in GDP
           </Box>
           <Box>
-            <Chart className="w-[70%] mx-auto  stroke-[1.6]" />
-            0.03% rise in water level
+            <ChartBarIcon className="w-[70%] mx-auto" />
+            3.6mm/year increase in sea level
+          </Box>
+          <Box>
+            <UserGroupIcon className="w-[70%] mx-auto" />
+            175M+ people affected
           </Box>
         </div>
       </div>
@@ -26,7 +30,7 @@ export default function GlobalStats() {
 }
 function Box({ children }) {
   return (
-    <div className="transition cursor-pointer text-center w-48 rounded-2xl bg-primary p-6 text-secondary dark:bg-secondary dark:text-primary ring-8 ring-primary/20 dark:ring-secondary/20 text-xl hover:scale-[0.98] flex-shrink-0 flex flex-col gap-4 justify-evenly m-4 sm:m-6">
+    <div className="transition cursor-pointer text-center w-48 rounded-2xl bg-primary p-6 text-secondary dark:bg-secondary dark:text-primary ring-8 ring-primary/20 dark:ring-secondary/20 text-xl hover:scale-[0.98] flex-shrink-0 flex flex-col gap-4 justify-evenly m-4 sm:m-6 font-bold">
       {children}
     </div>
   );
