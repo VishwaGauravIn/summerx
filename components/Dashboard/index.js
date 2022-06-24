8caf26b5588a0bbf1203ea2b3137e2e2ebfb74c0
Vishwa Gaurav <81325730+VishwaGauravIn@users.noreply.github.com>
2022-06-24 16:53:32 +0530

    responsiveness+

--- components/Dashboard/index.js
+++ components/Dashboard/index.js
@@ -6,7 +6,7 @@ import Widgets from "./widgets";
 
 export default function Dashboard() {
   return (
-    <div className="md:p-16 w-full relative">
+    <div className="py-10 sm:px-8 md:p-16 w-full relative">
       <Greeting />
       <div className="flex flex-col lg:flex-row  w-full">
         <div className="flex flex-col flex-wrap py-4 sm:p-4 items-center w-full lg:w-7/12">
