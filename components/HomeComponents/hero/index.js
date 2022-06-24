e2322e4460b8abecdc4ecdc208c33de2fbc7271d
Vishwa Gaurav <81325730+VishwaGauravIn@users.noreply.github.com>
2022-06-24 21:59:58 +0530

    added webimg

--- components/HomeComponents/hero/index.js
+++ components/HomeComponents/hero/index.js
@@ -37,7 +37,7 @@ export default function Hero() {
     <div className="flex flex-col md:flex-row mt-8 lg:mt-12 xl:mt-24 justify-center dark:text-zinc-200 text-primary">
       <div className="basis-6/12 flex flex-col gap-4 lg:gap-3 xl:gap-8 sm:pl-3 lg:pl-6 py-8 sm:py-10 md:py-12">
         <div>
-          <p className="text-2xl sm:text-3xl md:text-2xl lg:text-3xl xl:text-4xl font-light opacity-70 mb-1 sm:mb-2 md:mb-3 lg:mb-4 xl:mb-5 2xl:mb-6">
+          <p className="text-2xl sm:text-3xl md:text-2xl lg:text-3xl xl:text-4xl font-normal opacity-70 mb-1 sm:mb-2 md:mb-3 lg:mb-4 xl:mb-5 2xl:mb-6">
             We are on a Mission
           </p>
           <p className="text-5xl sm:text-7xl md:text-6xl lg:text-7xl xl:text-8xl font-bold capitalize">
