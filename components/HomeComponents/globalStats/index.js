f0c7b358335962ca033ca9438ce3d8efc411c429
Vishwa Gaurav <81325730+VishwaGauravIn@users.noreply.github.com>
2022-06-24 20:13:47 +0530

    minor design impv

--- components/HomeComponents/globalStats/index.js
+++ components/HomeComponents/globalStats/index.js
@@ -6,17 +6,17 @@ export default function GlobalStats() {
     <>
       <div className="text-center text-xl lg:text-3xl xl:text-4xl my-12 lg:my-24 xl:my-36 font-semibold">
         What&apos;s happening around globe due to Summer (per year)
-        <div className="flex flex-col md:flex-row justify-center items-center md:items-stretch gap-4 md:gap-6 lg:gap-8 xl:gap-12 w-[90%] mt-8 mx-auto flex-wrap  lg:mt-20">
+        <div className="flex flex-col md:flex-row justify-center items-center md:items-stretch w-[90%] mt-8 mx-auto flex-wrap  lg:mt-20">
           <Box>
-            <Person_Left className="w-[70%] mt-0 mx-auto stroke-[1.6]" />
+            <Person_Left className="w-[70%] mx-auto stroke-[1.6]" />
             160K+ lost their lives
           </Box>
           <Box>
-            <Fall className="w-[70%] mt-0 mx-auto  stroke-[1.6]" />
+            <Fall className="w-[70%] mx-auto  stroke-[1.6]" />
             5% fall in GDP
           </Box>
           <Box>
-            <Chart className="w-[70%] mt-0 mx-auto  stroke-[1.6]" />
+            <Chart className="w-[70%] mx-auto  stroke-[1.6]" />
             0.03% rise in water level
           </Box>
         </div>
@@ -26,7 +26,7 @@ export default function GlobalStats() {
 }
 function Box({ children }) {
   return (
-    <div className="transition cursor-pointer text-center w-48 rounded-2xl bg-primary p-6 text-secondary dark:bg-secondary dark:text-primary ring-8 ring-primary/20 dark:ring-secondary/20 text-xl hover:scale-[0.98] flex-shrink-0 flex flex-col gap-4 justify-evenly">
+    <div className="transition cursor-pointer text-center w-48 rounded-2xl bg-primary p-6 text-secondary dark:bg-secondary dark:text-primary ring-8 ring-primary/20 dark:ring-secondary/20 text-xl hover:scale-[0.98] flex-shrink-0 flex flex-col gap-4 justify-evenly m-4 sm:m-6">
       {children}
     </div>
   );
