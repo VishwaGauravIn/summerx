e3f69d439d64dff0fe1b23ea58377d6d92f9887f
Vishwa Gaurav <81325730+VishwaGauravIn@users.noreply.github.com>
2022-06-24 18:36:20 +0530

    title text impv

--- components/HomeComponents/hero/index.js
+++ components/HomeComponents/hero/index.js
@@ -3,43 +3,29 @@ import { AnimateText } from "../../elements/AnimatedText";
 
 export default function Hero() {
   return (
-    <div className="flex flex-col md:flex-row mt-8 lg:mt-12 xl:mt-24 justify-center">
-      <div className="basis-2/3 flex flex-col gap-4 lg:gap-3 xl:gap-8 pl-3 lg:pl-6 py-12">
+    <div className="flex flex-col md:flex-row mt-8 lg:mt-12 xl:mt-24 justify-center dark:text-zinc-200 text-primary">
+      <div className="basis-6/12 flex flex-col gap-4 lg:gap-3 xl:gap-8 sm:pl-3 lg:pl-6 py-8 sm:py-10 md:py-12">
         <div>
-          <div className="text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl font-medium text-primary-400 dark:text-secondary-600">
+          <div className="text-2xl sm:text-3xl md:text-2xl lg:text-3xl xl:text-4xl font-light opacity-70 mb-1 sm:mb-2 md:mb-3 lg:mb-4 xl:mb-5 2xl:mb-6">
             We are on a Mission
           </div>
-          <br />
-          <div className="text-5xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl font-bold capitalize">
+          <div className="text-5xl sm:text-7xl md:text-6xl lg:text-7xl xl:text-8xl font-bold capitalize">
             To Save lives
           </div>
         </div>
         <div>
-          <div className="text-xl md:text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl font-normal text-primary-500 dark:text-secondary-600">
+          {/* <div className="2xl:text-4xl font-normal mt-10">
             Occured due to
           </div>
-          <div className="text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl font-medium my-4">
+          <div className="2xl:text-7xl font-medium my-4">
             <AnimateText
               texts={["Summer", "Excssive Heat", "Dehydration", "Illness"]}
             />
-          </div>
-          <div className="flex flex-col md:flex-row justify-center md:justify-start gap-4 mt-8 lg:mt-12 xl:mt-16">
-            <a
-              href="#"
-              className="no-underline text-2xl xl:text-3xl px-4 py-2 rounded-full text-secondary-500 bg-primary-500 dark:bg-secondary-500 dark:text-primary-500 text-center outline outline-4 outline-secondary-600 dark:outline-primary-400"
-            >
-              Primary button
-            </a>
-            <a
-              href="#"
-              className="no-underline text-2xl xl:text-3xl px-4 py-2 rounded-full text-primary-400 bg-secondary-600 dark:bg-secondary-100 dark:text-primary-500 text-center outline outline-4 outline-secondary-900 dark:outline-primary-100"
-            >
-              Secondary button
-            </a>
-          </div>
+          </div> */}
+          {/* TODO: Buttons Design */}
         </div>
       </div>
-      <div className="basis-1/3 text-center p-4 w-[80%] lg:w-[90%] mt-0 mx-auto animate-bouncy ">
+      <div className="basis-5/12 text-center p-4 w-[80%] lg:w-[90%] mt-0 mx-auto animate-bouncy ">
         <img src="/hero.svg" alt="" className="hidden dark:block" />
         <img src="/hero_dark.svg" alt="" className="block dark:hidden" />
       </div>
