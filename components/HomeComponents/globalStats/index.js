d8675ff8f8c5f9a97da1f0db23c8dc273dbea94d
Vishwa Gaurav <81325730+VishwaGauravIn@users.noreply.github.com>
2022-06-24 18:05:16 +0530

    global stats impv

--- components/HomeComponents/globalStats/index.js
+++ components/HomeComponents/globalStats/index.js
@@ -4,7 +4,7 @@ import { Chart, Fall, Person_Left } from "../../elements/SVGs";
 export default function GlobalStats() {
   return (
     <>
-      <div className="text-center text-xl lg:text-3xl xl:text-4xl my-12 lg:my-24 xl:my-36">
+      <div className="text-center text-xl lg:text-3xl xl:text-4xl my-12 lg:my-24 xl:my-36 font-semibold">
         What&apos;s happening around globe due to Summer (per year)
         <div className="flex flex-col md:flex-row justify-center items-center md:items-stretch gap-4 md:gap-6 lg:gap-8 xl:gap-12 w-[90%] mt-8 mx-auto flex-wrap  lg:mt-20">
           <Box>
@@ -26,7 +26,7 @@ export default function GlobalStats() {
 }
 function Box({ children }) {
   return (
-    <div className="transition cursor-pointer text-center w-48 rounded-2xl bg-primary p-6 text-secondary dark:bg-secondary dark:text-primary ring-8 ring-primary/20 dark:ring-secondary/20 text-xl hover:scale-95 flex-shrink-0 flex flex-col gap-4 justify-evenly ">
+    <div className="transition cursor-pointer text-center w-48 rounded-2xl bg-primary p-6 text-secondary dark:bg-secondary dark:text-primary ring-8 ring-primary/20 dark:ring-secondary/20 text-xl hover:scale-[0.98] flex-shrink-0 flex flex-col gap-4 justify-evenly">
       {children}
     </div>
   );
