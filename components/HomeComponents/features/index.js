6dfa2ef63ae2fbd9455dbdacba0a884e4fb8cfe7
Vishwa Gaurav <81325730+VishwaGauravIn@users.noreply.github.com>
2022-06-24 18:38:13 +0530

    better code

--- components/HomeComponents/features/index.js
+++ components/HomeComponents/features/index.js
@@ -6,9 +6,9 @@ export default function Features() {
     <div>
       <div className="text-center text-xl lg:text-3xl xl:text-4xl mt-12 lg:mt-24 xl:mt-36  ">
         No Need To Worry!
-        <div className="font-bold text-primary-900  dark:text-secondary-200 my-4 text-3xl lg:text-6xl">
+        <p className="font-bold text-primary-900  dark:text-secondary-200 my-4 text-3xl lg:text-6xl">
           Here is Best and Simple Solution
-        </div>
+        </p>
         <div className="grid grid-cols-1 md:grid-cols-2 justify-center items-center gap-4 lg:gap-6 xl:gap-8 w-[90%] mt-8 mx-auto lg:mt-20">
           {[
             "Heat wave warning",
@@ -21,11 +21,10 @@ export default function Features() {
           ].map((name) => (
             <Box key={name}>{name}</Box>
           ))}
-          <div className="flex flex-row items-start gap-4 ">All for FREE!</div>
+          <p className="flex flex-row items-start gap-4 ">All for FREE!</p>
         </div>
       </div>
       <div className="my-12 py-8 text-center">
-        {" "}
         <a
           href="#"
           className="no-underline text-3xl xl:text-4xl  px-8 py-4 rounded-full text-secondary-500 bg-primary-500 dark:bg-secondary-500 dark:text-primary-500 text-center outline outline-4 outline-secondary-600 dark:outline-primary-400 "
--- components/HomeComponents/hero/index.js
+++ components/HomeComponents/hero/index.js
@@ -6,12 +6,12 @@ export default function Hero() {
     <div className="flex flex-col md:flex-row mt-8 lg:mt-12 xl:mt-24 justify-center dark:text-zinc-200 text-primary">
       <div className="basis-6/12 flex flex-col gap-4 lg:gap-3 xl:gap-8 sm:pl-3 lg:pl-6 py-8 sm:py-10 md:py-12">
         <div>
-          <div className="text-2xl sm:text-3xl md:text-2xl lg:text-3xl xl:text-4xl font-light opacity-70 mb-1 sm:mb-2 md:mb-3 lg:mb-4 xl:mb-5 2xl:mb-6">
+          <p className="text-2xl sm:text-3xl md:text-2xl lg:text-3xl xl:text-4xl font-light opacity-70 mb-1 sm:mb-2 md:mb-3 lg:mb-4 xl:mb-5 2xl:mb-6">
             We are on a Mission
-          </div>
-          <div className="text-5xl sm:text-7xl md:text-6xl lg:text-7xl xl:text-8xl font-bold capitalize">
+          </p>
+          <p className="text-5xl sm:text-7xl md:text-6xl lg:text-7xl xl:text-8xl font-bold capitalize">
             To Save lives
-          </div>
+          </p>
         </div>
         <div>
           {/* <div className="2xl:text-4xl font-normal mt-10">
