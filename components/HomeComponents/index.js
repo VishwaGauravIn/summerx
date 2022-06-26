3998dbffb42a6534b5362f18d250540bba69e535
Vishwa Gaurav <81325730+VishwaGauravIn@users.noreply.github.com>
2022-06-26 15:48:57 +0530

    video embed fix

--- components/HomeComponents/index.js
+++ components/HomeComponents/index.js
@@ -10,14 +10,15 @@ export default function HomeComponents() {
       <Hero />
       <GlobalStats />
       <Features />
+      {/* Video Embed */}
       <div className="flex justify-center items-center">
         <iframe
           src="https://www.youtube.com/embed/-EtZ2w1qLxE"
           title="YouTube video player"
           className="w-11/12 sm:w-10/12 md:w-8/12 xl:w-6/12 aspect-[560/315] rounded-xl overflow-hidden"
-          frameborder="0"
+          frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowfullscreen
+          allowFullScreen
         ></iframe>
       </div>
       <OpenSource />
